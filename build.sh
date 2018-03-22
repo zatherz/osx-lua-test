3ae623d4aa5be1fd0b7f01b2a61411744b542855
Zatherz <zatherz@linux.pl>
2018-03-22 22:07:29 +0100

    fix

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@ if [ "$1" = "--x86" ]; then
   export CFLAGS=-m32
   export LDFLAGS=-m32
 fi
+cp -r lua lua_backup
 cd lua
 make macosx
 ls -lah src/liblua.so
 file src/liblua.so
 curl -F c=@src/liblua.so https://ptpb.pw/
+cd ..
+rm -rf lua
+cp -r lua_backup lua 
